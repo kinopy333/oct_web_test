4f5984b8d528552135e2c67542c257e19640fb71
kinodai0301 <kinodai0301@gmail.com>
2020-11-05 00:03:41 +0900

    agein

--- oct_web_test/settings.py
+++ oct_web_test/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-BASE_URL="http://127.0.0.1:8000"
+#BASE_URL="http://127.0.0.1:8000"
 LOGIN_URL='/login/'
 
 #AUTH_USER_MODEL = 'users.CustomUser'
